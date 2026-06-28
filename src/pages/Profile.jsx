08a09aa47b2a8ec422e18cb5c036fe6e44c44784
Wunyo99 <mawunyoanani4@gmail.com>
2026-06-28 22:56:01 +0000

    UI update in profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -132,165 +132,190 @@ const Profile = () => {
 
   return (
     <>
-      <section className="section-padding grid grid-cols-1 md:grid-cols-2  gap-2">
-        <div className="bg-white border-2 border-gray-200 rounded-2xl lg:w-[80%] p-2 flex flex-col">
-          <div className="relative">
-            <div className="bg-purple-800 flex items-center justify-center h-40 rounded-2xl">
-              <p className="font-medium text-7xl text-purple-300">{user.job}</p>
-            </div>
-
-            <div className="absolute top-22 left-5">
-              <div className="relative w-30 h-30">
-                <img
-                  src={user?.photoURL || "/default-avatar.png"}
-                  className="w-full h-full rounded-full border-4 border-white object-cover"
-                />
-
-                <button
-                  onClick={() => fileRef.current.click()}
-                  className="absolute bottom-1 right-1 bg-white p-2 rounded-full shadow-md hover:scale-105 transition"
-                >
-                  <Pencil size={16} className="text-purple-800" />
-                </button>
+      <section className="section-padding ">
+        <h4 className="text-2xl md:text-4xl font-medium mb-5 text-center md:text-start">
+          Welcome back,{" "}
+          <span className="text-purple-800">{user.firstname}</span>{" "}
+        </h4>
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-7">
+          <div className="bg-white border-2 border-gray-200 rounded-2xl lg:w-[80%] p-2 flex flex-col">
+            <div className="relative">
+              <div className="bg-purple-800 flex justify-center h-40 rounded-2xl">
+                <p className="font-medium text-3xl text-center mt-5 text-purple-300">
+                  {user.job}
+                </p>
               </div>
 
-              <input
-                ref={fileRef}
-                type="file"
-                accept="image/*"
-                className="hidden"
-                onChange={handleImageSelect}
-              />
-            </div>
-          </div>
+              <div className="absolute top-22 left-5">
+                <div className="relative w-30 h-30">
+                  <img
+                    src={user?.photoURL || "/default-avatar.png"}
+                    className="w-full h-full rounded-full border-4 border-white object-cover"
+                  />
 
-          <div className="mt-12">
-            <div className="flex items-center gap-1">
-              {isEditing ? (
-                <div className="flex gap-2">
+                  <button
+                    onClick={() => fileRef.current.click()}
+                    className="absolute bottom-1 right-1 bg-white p-2 rounded-full shadow-md hover:scale-105 transition"
+                  >
+                    <Pencil size={16} className="text-purple-800" />
+                  </button>
+                </div>
+
+                <input
+                  ref={fileRef}
+                  type="file"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={handleImageSelect}
+                />
+              </div>
+            </div>
+
+            <div className="mt-12">
+              <div className="flex items-center gap-1">
+                {isEditing ? (
+                  <div className="flex gap-2">
+                    <input
+                      name="firstname"
+                      value={formData.firstname}
+                      onChange={handleChange}
+                      className="py-3 px-5 w-full bg-gray-200 border-0 focus:outline-purple-500 rounded-lg"
+                      placeholder="First name"
+                    />
+                    <input
+                      name="lastname"
+                      value={formData.lastname}
+                      onChange={handleChange}
+                      className="py-3 px-5 w-full bg-gray-200 border-0 focus:outline-purple-500 rounded-lg"
+                      placeholder="Last name"
+                    />
+                  </div>
+                ) : (
+                  <h1 className="text-2xl font-medium text-gray-900">
+                    {user.firstname} {user.lastname}
+                  </h1>
+                )}
+                {isEditing ? (
+                  ""
+                ) : (
+                  <BadgeCheck size={15} className="text-green-600" />
+                )}
+              </div>
+
+              <div className="mt-5">
+                <h1 className="font-medium">Bio</h1>
+
+                {isEditing ? (
+                  <textarea
+                    name="bio"
+                    value={formData.bio}
+                    onChange={handleChange}
+                    className="py-3 px-5 w-full bg-gray-200 border-0 focus:outline-purple-500 rounded-lg"
+                    placeholder="Write your bio..."
+                  />
+                ) : (
+                  <p>{user.bio || "No bio added yet."}</p>
+                )}
+              </div>
+
+              <div className="mt-5 flex items-center gap-4">
+                {isEditing ? (
                   <input
                     name="firstname"
-                    value={formData.firstname}
+                    value={formData.phone}
                     onChange={handleChange}
                     className="py-3 px-5 w-full bg-gray-200 border-0 focus:outline-purple-500 rounded-lg"
-                    placeholder="First name"
+                    placeholder="phone"
                   />
-                  <input
-                    name="lastname"
-                    value={formData.lastname}
-                    onChange={handleChange}
-                    className="py-3 px-5 w-full bg-gray-200 border-0 focus:outline-purple-500 rounded-lg"
-                    placeholder="Last name"
-                  />
-                </div>
-              ) : (
-                <h1 className="text-2xl font-medium text-gray-900">
-                  {user.firstname} {user.lastname}
-                </h1>
-              )}
-              {isEditing ? (
-                ""
-              ) : (
-                <BadgeCheck size={15} className="text-green-600" />
-              )}
-            </div>
+                ) : (
+                  <div className="border-2 border-gray-800 rounded-full p-2 flex items-center gap-2">
+                    <Phone className="text-purple-800" size={25} />
+                    <p>{user.phone}</p>
+                  </div>
+                )}
 
-            <div className="mt-5">
-              <h1 className="font-medium">Bio</h1>
-
-              {isEditing ? (
-                <textarea
-                  name="bio"
-                  value={formData.bio}
-                  onChange={handleChange}
-                  className="py-3 px-5 w-full bg-gray-200 border-0 focus:outline-purple-500 rounded-lg"
-                  placeholder="Write your bio..."
-                />
-              ) : (
-                <p>{user.bio || "No bio added yet."}</p>
-              )}
-            </div>
-
-            <div className="mt-5 flex items-center gap-4">
-              {isEditing ? (
-                <input
-                  name="firstname"
-                  value={formData.phone}
-                  onChange={handleChange}
-                  className="py-3 px-5 w-full bg-gray-200 border-0 focus:outline-purple-500 rounded-lg"
-                  placeholder="phone"
-                />
-              ) : (
                 <div className="border-2 border-gray-800 rounded-full p-2 flex items-center gap-2">
-                  <Phone className="text-purple-800" size={25} />
-                  <p>{user.phone}</p>
+                  <MdEmail className="text-purple-800" size={25} />
+                  <p>{user.email}</p>
                 </div>
-              )}
+              </div>
 
-              <div className="border-2 border-gray-800 rounded-full p-2 flex items-center gap-2">
-                <MdEmail className="text-purple-800" size={25} />
-                <p>{user.email}</p>
+              <div className="mt-4 flex items-center gap-3">
+                {isEditing ? (
+                  <>
+                    <button
+                      onClick={handleSave}
+                      className="bg-purple-200 text-sm text-purple-800 font-medium px-2 py-1 rounded-full flex items-center gap-1 cursor-pointer hover:scale-98 duration-200"
+                    >
+                      Save
+                    </button>
+
+                    <button
+                      onClick={() => {
+                        setFormData({
+                          firstname: user.firstname,
+                          lastname: user.lastname,
+                          bio: user.bio || "",
+                          phone: user.phone || "",
+                        });
+
+                        setIsEditing(false);
+                      }}
+                      className="border border-gray-300 text-red-600 cursor-pointer p-2 rounded-lg hover:bg-gray-100"
+                    >
+                      <X size={15} />
+                    </button>
+                  </>
+                ) : (
+                  <div className="py-2">
+                    <button
+                      onClick={() => setIsEditing(true)}
+                      className="bg-purple-200 text-purple-800 font-medium px-2 py-1 rounded-full flex items-center gap-1 cursor-pointer hover:scale-98 duration-200"
+                    >
+                      <Pencil size={15} />
+                      <span className="text-sm">Edit Profile</span>
+                    </button>
+                  </div>
+                )}
               </div>
             </div>
+          </div>
 
-            <div className="mt-4 flex items-center gap-3">
-              {isEditing ? (
-                <>
-                  <button
-                    onClick={handleSave}
-                    className="bg-purple-200 text-sm text-purple-800 font-medium px-2 py-1 rounded-full flex items-center gap-1 cursor-pointer hover:scale-98 duration-200"
-                  >
-                    Save
-                  </button>
-
-                  <button
-                    onClick={() => {
-                      setFormData({
-                        firstname: user.firstname,
-                        lastname: user.lastname,
-                        bio: user.bio || "",
-                        phone: user.phone || "",
-                      });
-
-                      setIsEditing(false);
-                    }}
-                    className="border border-gray-300 text-red-600 cursor-pointer p-2 rounded-lg hover:bg-gray-100"
-                  >
-                    <X size={15} />
-                  </button>
-                </>
-              ) : (
-                <div className="py-2">
-                  <button
-                    onClick={() => setIsEditing(true)}
-                    className="bg-purple-200 text-purple-800 font-medium px-2 py-1 rounded-full flex items-center gap-1 cursor-pointer hover:scale-98 duration-200"
-                  >
-                    <Pencil size={15} />
-                    <span className="text-sm">Edit Profile</span>
-                  </button>
+          <div className="flex flex-col">
+            <div className="mb-2">
+              <div className="flex items-center gap-4">
+                {" "}
+                <h1 className="text-3xl font-medium">Your Saved Jobs</h1>
+                <div className="w-5 h-5 flex items-center justify-center text-center bg-purple-200 rounded-full">
+                  <p className=" text-purple-800 font-medium">
+                    {savedJobs.length}
+                  </p>
                 </div>
+              </div>
+              <div className="h-1 w-15 bg-purple-800" />
+            </div>
+            <div>
+              {isLoading ? (
+                <Spinner />
+              ) : savedJobs.length > 0 ? (
+                <div className="border-2 border-gray-200 flex flex-col gap-4 p-5 rounded-2xl overflow-y-scroll h-80">
+                  <JobCard jobs={savedJobs} />
+                </div>
+              ) : (
+                <p className="text-center text-gray-500">
+                  You haven't saved any jobs yet.
+                </p>
               )}
             </div>
+            <button
+              onClick={logoutNavigateSignIn}
+              className="bg-red-200 text-red-600 font-medium px-4 py-3 rounded-lg mt-7 w-80 mx-auto cursor-pointer hover:scale-98 duration-200"
+            >
+              Logout
+            </button>
           </div>
         </div>
-        <div>
-          {isLoading ? (
-            <Spinner />
-          ) : savedJobs.length > 0 ? (
-            <JobCard jobs={savedJobs} />
-          ) : (
-            <p className="text-center text-gray-500">
-              You haven't saved any jobs yet.
-            </p>
-          )}
-        </div>
       </section>
-      <button
-        onClick={logoutNavigateSignIn}
-        className="bg-red-500 text-white px-4 py-2 rounded-lg"
-      >
-        Logout
-      </button>
     </>
   );
 };
